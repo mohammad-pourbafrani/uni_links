69d4de84bfebbbc097be9131e5fd8b821a80a21e
mohammad pourbafrani <79800704+mohammad-pourbafrani@users.noreply.github.com>
2025-05-05 17:13:44 +0330

    Update UniLinksPlugin.java compatible with flutter 3.29.2

--- android/src/main/java/name/avioli/unilinks/UniLinksPlugin.java
+++ android/src/main/java/name/avioli/unilinks/UniLinksPlugin.java
@@ -80,20 +80,20 @@ public class UniLinksPlugin
         eventChannel.setStreamHandler(plugin);
     }
 
-    /** Plugin registration. */
-    public static void registerWith(@NonNull PluginRegistry.Registrar registrar) {
-        // Detect if we've been launched in background
-        if (registrar.activity() == null) {
-            return;
-        }
+    // /** Plugin registration. */
+    // public static void registerWith(@NonNull PluginRegistry.Registrar registrar) {
+    //     // Detect if we've been launched in background
+    //     if (registrar.activity() == null) {
+    //         return;
+    //     }
 
-        final UniLinksPlugin instance = new UniLinksPlugin();
-        instance.context = registrar.context();
-        register(registrar.messenger(), instance);
+    //     final UniLinksPlugin instance = new UniLinksPlugin();
+    //     instance.context = registrar.context();
+    //     register(registrar.messenger(), instance);
 
-        instance.handleIntent(registrar.context(), registrar.activity().getIntent());
-        registrar.addNewIntentListener(instance);
-    }
+    //     instance.handleIntent(registrar.context(), registrar.activity().getIntent());
+    //     registrar.addNewIntentListener(instance);
+    // }
 
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {}
